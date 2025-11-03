3d54b1d59a014a0a2c889408c988bee6de0be915
Bok Jun Ho <junho4560@naver.com>
2025-11-03 11:24:00 +0900

    1단계 구현

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,8 +1,23 @@
 package com.back;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+
+        System.out.println("== 명언 앱 ==");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
+
+        sc.close();
     }
 }
